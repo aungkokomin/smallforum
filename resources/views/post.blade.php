ba7839d7a0da860a63622f0b0e7f378f611d6093
Ko Min <aungkokomin.webdeveloper@gmail.com>
2021-08-13 01:28:32 +0630

    Add post grid

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,9 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
         <x-post-feature :post="$posts[0]" />
-            <div class="lg:grid lg:grid-cols-2">
-                @foreach ($posts->skip(1) as $post)
-                    <x-post-card :post="$post" />    
-                @endforeach
-            </div>
+        
+        <x-post-grid :post="posts" />
+            
         @endif
     </main>
     {{-- @foreach($posts as $post)
